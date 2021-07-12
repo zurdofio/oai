60e39d247dab712df8f22f99cac2d4e7345198a4
Mohammed Ismail <mohammed.ismail@openairinterface.org>
2021-07-12 10:33:35 +0200

    CI: docker-compose updated on the SMF to add fqdn env variables
    Signed-off-by: Mohammed Ismail <mohammed.ismail@openairinterface.org>

--- ci-scripts/dsTesterDockerCompose/smf-healthy-check.sh
+++ ci-scripts/dsTesterDockerCompose/smf-healthy-check.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 STATUS=0
-NB_UNREPLACED_AT=`cat /openair-smf/etc/*.conf | grep -v contact@openairinterface.org | grep -c @ || true`
-NOT_FOUND=`ldd /openair-smf/bin/oai_smf | grep -c "not found" || true`
-if [[ $NB_UNREPLACED_AT -ne 0 || $NOT_FOUND -ne 0 ]]
-then
-  STATUS=-1
+
+RESULT=$(ps aux | grep -v nohup || true)
+SUB='/openair-smf/bin/oai_smf -c /openair-smf/etc/smf.conf -o'
+if [[ $RESULT =~ $SUB ]]; then
+    STATUS=0
+else
+   STATUS=-1
 fi
 
 exit $STATUS
