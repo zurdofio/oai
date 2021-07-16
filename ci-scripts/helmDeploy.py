c6b560968f558b0161c631ac6aa7abdedba90606
ismail <mohammed.ismail@openairinterface.org>
2021-07-16 14:05:11 +0200

    CI: fix on directory
    Signed-off-by: ismail <mohammed.ismail@openairinterface.org>

--- ci-scripts/helmDeploy.py
+++ ci-scripts/helmDeploy.py
@@ -43,11 +43,6 @@ logging.basicConfig(
     format="[%(asctime)s] %(name)s:%(levelname)s: %(message)s"
 )
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="[%(asctime)s] %(name)s:%(levelname)s: %(message)s"
-)
-
 class ClusterDeploy:
 	def __init__(self):
 		self.OCUserName = ""
